00ccc76b7a1f9ac5cfcf329481d003f3d5f765ac
Salvatori-Mathias <salvatorimathias@gmail.com>
2026-06-26 18:17:45 +0200

    fix: update menu navigation style and layout

--- moto-clockv2/src/components/layout/NavBar.jsx
+++ moto-clockv2/src/components/layout/NavBar.jsx
@@ -9,11 +9,11 @@ export const NavBar = ({ isMenuOpen, setIsMenuOpen, onNavigate }) => {
       </button>
 
       <ul className="menu-links">
-        <li onClick={() => { onNavigate('planning'); setIsMenuOpen(false); }}>Line-up</li>
-        <li onClick={() => { onNavigate('map'); setIsMenuOpen(false); }}>Plan du Site</li>
-        <li onClick={() => { onNavigate('kit'); setIsMenuOpen(false); }}>Survival-Kit</li>
-          <li onClick={() => { onNavigate('securite'); setIsMenuOpen(false); }}>Objets autorisés/ interdis</li>
-      </ul>
+  <li className="menu-item" onClick={() => { onNavigate('planning'); setIsMenuOpen(false); }}>Line-up</li>
+  <li className="menu-item" onClick={() => { onNavigate('map'); setIsMenuOpen(false); }}>Plan du Site</li>
+  <li className="menu-item" onClick={() => { onNavigate('kit'); setIsMenuOpen(false); }}>Survival-Kit</li>
+  <li className="menu-item" onClick={() => { onNavigate('securite'); setIsMenuOpen(false); }}>Objets autorisés/ interdis</li>
+</ul>
     </nav>
   );
 };
